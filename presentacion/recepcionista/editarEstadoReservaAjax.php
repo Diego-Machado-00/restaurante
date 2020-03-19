576ba9aa8ca2cb4f1fac522cb14c7c3716c0faf2
Camilo <camiwap6@gmail.com>
2020-03-19 00:45:53 -0500

    editarestadoAjax

--- presentacion/recepcionista/editarEstadoReservaAjax.php
+++ presentacion/recepcionista/editarEstadoReservaAjax.php
@@ -1,4 +1,4 @@
 <?php
-$reserva = new Reserva($_GET['idReserva'],"", "",$_GET['cl'], $mesa_idmesa="", $recepcionista_idrecepcionista="" , $estado="" );
+$reserva = new Reserva($_GET['idReserva'],"", "",$_GET['cl'], $_GET[''], $_SESSION['id'],"" );
 $reserva->actualizarEstado();
 echo "<span class='fas " . ($reserva->getEstado()==0?"fa-times-circle":"fa-check-circle") . "' data-toggle='tooltip' class='tooltipLink' data-placement='left' data-original-title='" . ($reserva->getEstado()==0?"Inhabilitado":"Habilitado") . "' ></span>";
